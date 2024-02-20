63a240db3a306f0a504f216a6c5c10203ef2b91e
Marc-Antoine Nadeau <marc-antoine.nadeau@mail.mcgill.ca>
2024-02-20 11:52:08 -0500

    Added stuff ater OH

--- Part2/part2-recursive_V2.s
+++ Part2/part2-recursive_V2.s
@@ -18,18 +18,20 @@ stop:
 pell:
 // your code starts here
     // if (n != 1) || (n != 2)
+    PUSH {V1,LR}
     CMP  A1, #2 // compares A1 (n) with immediate 2
     BGT recursive_case // If A1 (n) > 2 , go to recursive
-    BX LR                // Return from function
+    B end_pell               // Return from function
 
 recursive_case:
-    SUB A1, A1, #1 // A2 (n - 1) = A1 (n) - 1 
+    MOV V1, A1 
+    SUB A1, V1, #1 // A2 (n - 1) = A1 (n) - 1 
     BL pell // recursive -> pell(n-1)
 
     PUSH {A1, LR} // Saves A1 on the stack
 
     // pell(n-2)
-    SUB A1, A1, #1   // A1 (n-2) = A1 (n-1) - 1
+    SUB A1, V1, #2   // A1 (n-2) = A1 (n-1) - 1
     BL pell          // recursive call for pell(n-2)
 
     
@@ -43,6 +45,9 @@ recursive_case:
 
     // Restore LR from the stack
     POP {LR}
+    POP {V1}
 
     // Return to the calling function
     BX LR
+end_pell:
+    POP {V1, PC}
