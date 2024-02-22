1f6b42a63b088139ff5f804641a721071c429171
Marc-Antoine Nadeau <marc-antoine.nadeau@mail.mcgill.ca>
2024-02-22 15:22:48 -0500

    bubble works

--- Part3/BubbleSort.c
+++ Part3/BubbleSort.c
@@ -1,46 +1,29 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void swap(char* xp, char* yp) {
-    char temp = *xp;
+ 
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
     *xp = *yp;
     *yp = temp;
 }
-
-// An optimized version of Bubble Sort for sorting strings
-char* sort(char *input, int n) {
-    char *sorted = (char*)malloc((n + 1) * sizeof(char)); // Allocate memory for sorted string
-    if (sorted == NULL) {
-        printf("Memory allocation failed!\n");
-        exit(1);
-    }
-    strcpy(sorted, input); // Copy input string to sorted string
-
+ 
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
     bool swapped;
-    for (int i = 0; i < n - 1; i++) {
+    for (i = 0; i < n - 1; i++) {
         swapped = false;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (sorted[j] > sorted[j + 1]) {
-                swap(&sorted[j], &sorted[j + 1]);
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
                 swapped = true;
             }
         }
-        // If no two elements were swapped by inner loop,
-        // then break
-        if (!swapped)
+ 
+        // If no two elements were swapped by inner loop, then break
+        if (swapped == false)
             break;
     }
-
-    return sorted;
-}
-
-int main() {
-    char input[] = "bubbleSort";
-    int n = strlen(input);
-    char *sorted = sort(input, n);
-    printf("Sorted string: %s\n", sorted);
-    free(sorted); // Freeing the memory allocated by sort function
-    return 0;
 }
--- Part3/part3-BubbleSort.s
+++ Part3/part3-BubbleSort.s
@@ -19,5 +19,55 @@ stop:
 // pre-- A1: Address of array of signed bytes to be sorted
 // pre-- A2: Number of elements in array
 // post- A1: Address of sorted array
+
 sort:
-      // your code starts here
+	PUSH {V1-V7, LR} 		
+
+	MOV V1, #0				// V1 (i) = 0	
+
+	//MOV A4, #0 				// A4 (Swapped) = 0 (false)				
+	
+loop1:
+	SUB A3, A2, #1 			// A3 (n-1) = A2 (n) - 1
+	CMP V1, A3 				// Compare V1 (i) and A3 (n-1)
+	BGE end_loop			//End Loop if V1 (i) >= A3 (n-1)
+
+	//CMP A4, #1 				// Compare A4 (Swapped)  and #1
+	//BEQ end_loop			// if A4 (Swapped) == 1 (true), end sorting
+
+	MOV V2, #0				// V2 (j) = 0
+
+loop2:
+	SUB V3, A3, V1			// V3 ( n-i-1 ) = A3 (n - 1) - V1 (i) 
+	CMP V2, V3 				// Compare V2 (j) and V3 ( n - i - 1)
+	BGE loop1_cont			// Branch back to loop1_cont if V2 (j) >= V3 ( n - i - 1)
+
+	LDRSB V4, [A1, V2] 		// key (V4) = arr[j] 
+
+	ADD V5, V2, #1			// V5 (j+1) = V2 (j) + 1
+	LDRSB V6, [A1, V5] 		// key (V4) = arr[j+1] 
+	CMP V4, V6				// Compare V4 (arr[j]) and V6 (arr[j+1])
+	BLE loop2_cont			// if V4 (arr[j]) <= V4 (arr[j+1]) branch to loop1_cont
+
+	MOV V7, V4 				// tmp of arr[j]
+	STRB V6, [A1, V2]		// Stores arr[j+1] into arr[j]
+	STRB V7, [A1, V5]
+	
+	MOV A4, #1 				// Swapped = true
+
+	ADD V2, V2, #1 			// V2 (j + 1) = V2 (j) + 1
+	B loop2
+
+loop2_cont: 
+	ADD V2, V2, #1 			// V2 (j+1) = A2 (j) + 1
+
+loop1_cont:
+	CMP A4, #0
+	BEQ end_loop				
+	ADD V1, V1, #1 			// V1 (i+1) = V1 (i) + 1
+	B loop1					// branch back to loop1
+
+end_loop:
+	POP {V1-V7, LR}
+	BX LR
+
